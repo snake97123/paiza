1f1f7d31db9c23bec869a7394dd7686dfc542105
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-06-30 03:10:39 +0900

    商品検索　修正

--- dataset/paiza3.php
+++ dataset/paiza3.php
@@ -4,23 +4,23 @@
    $m = $input[1];
    
    $array = [];
+   
    for ($i = 0; $i < $n; $i++) {
         $array_element = trim(fgets(STDIN));
         
         array_push($array, $array_element);
    }
+//   print_r($array);
    
    for ($i = 0; $i < $m; $i++) {
         $search_element = trim(fgets(STDIN));
-        // $key = array_search($search_element, $array);
-        
-        if (in_array($search_element, $array)) {
-            // echo -1 . "\n";
-            $key = array_keys($array, $search_element);
-            echo  $key + 1 . "\n";
-        } else { 
-            // echo $key + 1 . "\n";
+        $key = array_search($search_element, $array);
+       
+        if($key || $key === 0){
+            echo $key + 1 . "\n";
+        } else {
             echo -1 . "\n";
         }
+        
    }
 ?>
