913eb83e123c519668eabc1f68b53addfdd773f4
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-07-01 21:07:17 +0900

    匿名希望さん

---
+++ ikusa/paiza5.php
@@ -0,0 +1,10 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+   $inputs = explode(" ", trim(fgets(STDIN)));
+   
+   $a = $inputs[0];
+   $b = $inputs[1];
+   
+   echo $a[0] . "." . $b[0] . ".";
+?>
