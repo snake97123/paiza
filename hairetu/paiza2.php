efbabaecd0062986917e0288e05386405d7111e8
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-07-04 06:02:27 +0900

    傾斜配点

--- hairetu/paiza2.php
+++ hairetu/paiza2.php
@@ -2,17 +2,18 @@
   $n = trim(fgets(STDIN));
 
   $sbjects_omomi = explode(" ", trim(fgets(STDIN)));
-  print_r($sbjects_omomi);
+//   print_r($sbjects_omomi);
   for ($i = 0; $i < $n; $i++) {
        $tensuu[] = explode(" ", trim(fgets(STDIN)));
   }
-  print_r($tensuu);
  
- for ($i = 0; $i < 1; $i++) {
+ 
+ for ($i = 0; $i < $n; $i++) {
+     $sum = 0;
      for ($j = 0; $j < 5; $j++) {
          $sum += $sbjects_omomi[$j] * $tensuu[$i][$j];
      } 
      $sums[] = $sum;
  }
- print_r($sums);
+     echo max($sums);
 ?>
