9a61ccb6b44623ba84be91d865a3bfe2881e5699
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-07-27 19:23:32 +0900

    Update paiza16.php

--- hayatoki/paiza16.php
+++ hayatoki/paiza16.php
@@ -5,7 +5,7 @@
         echo "quiet";
     } elseif($input >= 30 && $input < 50){
         echo "normal";
-    } elseif($input >=50 && $input < 70){
+    } elseif($input >= 50 && $input < 70){
         echo "noisy";
     } elseif($input >= 70) {
         echo "very noisy";
