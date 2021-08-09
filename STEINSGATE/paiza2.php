eccaa919ee7285ca84aaae33b200099a1e070a43
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-08-09 09:28:53 +0900

    性能解析タイプセーフコード修正

--- STEINSGATE/paiza2.php
+++ STEINSGATE/paiza2.php
@@ -1,17 +1,17 @@
 <?php
     $inputs = explode(" ", trim(fgets(STDIN)));
-    
+
     $characters = $inputs[0];
     $keikenti = $inputs[1];
     $need_keikenti = 0;
     $need_keikentis = 0;
-    
+
     for ($i = 0; $i < $characters; $i++) {
         $need_keikenti =  trim(fgets(STDIN));
-        $need_keikentis += $need_keikenti;
+        $need_keikentis += $need_keikenti * 10;
     }
-    
+
     // echo $need_keikenti;
-    $times = ceil($need_keikentis / $keikenti);
+    $times = ceil($need_keikentis / ($keikenti * 10));
     echo $times;
 ?>
