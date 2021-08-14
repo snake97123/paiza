1ea9c46f3a8d922443d987c29550163505ca535e
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-08-14 19:32:03 +0900

    数列の差

---
+++ STEINSGATE/tabusa-ti/paiza2.php
@@ -0,0 +1,17 @@
+<?php
+   $input = trim(fgets(STDIN));
+   $arrays1 = explode(" ", trim(fgets(STDIN)));
+   $arrays2 = explode(" ", trim(fgets(STDIN)));
+   
+   $new_array = array();
+   
+   for ($i = 0; $i < $input; $i++) {
+        $new_array[$i] = $arrays2[$i] - $arrays1[$i];
+   }
+   
+//   print_r($new_array);
+   foreach ($new_array as $value) {
+       echo $value . " "; 
+   }
+   
+?>
