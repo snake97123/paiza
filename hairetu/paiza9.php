8aa96d25e689b81c7195ea129f4b2786b29616f7
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-09-09 07:53:50 +0900

    指示入力まで

---
+++ hairetu/paiza9.php
@@ -0,0 +1,17 @@
+<?php
+    $input = explode(" ", trim(fgets(STDIN)));
+    $N = $input[0];
+    $Q = $input[1];
+    
+    $array = range(1,$N);
+    
+    // print_r($array);
+    for ($i = 0; $i < $Q; $i++) {
+         $order = explode(" ", trim(fgets(STDIN)));
+         
+        
+    }
+    
+    
+    
+?>
