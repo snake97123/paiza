07dddc1b86a1d2efcc03fd936e5f7703c2bc2ce3
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-06-30 08:02:09 +0900

    花言葉

---
+++ morinocontest4/paiza1.py
@@ -0,0 +1,12 @@
+flower_language = input()
+if flower_language == 'rose':
+    print('I love you')
+elif flower_language == 'jasmine':
+    print('amiability')
+elif flower_language == 'cosmos':
+    print('harmony')
+elif flower_language =='tulip':
+    print('perfect lover')
+elif flower_language == 'hydrangea':
+    print('heartlessness')
+    
