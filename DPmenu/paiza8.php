573a0921889d548b156fd2c876c16ff11fb0d52c
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-08-21 13:16:47 +0900

    階段の上り方２

---
+++ DPmenu/paiza8.php
@@ -0,0 +1,21 @@
+<?php 
+
+[$n, $a, $b] = explode(" ", trim(fgets(STDIN)));
+$DP = [];
+for($i = 1; $i <= $n; $i++){
+    $DP[$i] = 0;
+}
+$DP[0] = 1;
+
+for($i = 1; $i <= $n; $i++){
+    if($i >= $a){
+        $DP[$i] += $DP[$i - $a]; 
+    }
+    if($i >= $b){
+        $DP[$i] += $DP[$i - $b];
+    }
+    
+    
+}
+echo($DP[$n]);
+?>
