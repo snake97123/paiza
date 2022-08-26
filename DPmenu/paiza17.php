524b53b56402d897752baf86cf2810d58be6cea7
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-08-26 15:49:08 +0900

    最長減少部分列

---
+++ DPmenu/paiza17.php
@@ -0,0 +1,21 @@
+<?php 
+  
+  $n = trim(fgets(STDIN));
+  $trees = [];
+  for($i = 0; $i <= $n; $i++){
+    $trees[$i] = trim(fgets(STDIN));
+  }
+  $dp =[];
+  for($i = 0; $i < $n; $i++){
+    $dp[$i] = 1;
+  }
+  for($i = 1; $i < $n; $i++){
+    for($j = 0; $j < $i; $j++){
+      if ($trees[$j] > $trees[$i]){
+        $dp[$i] = max($dp[$i], $dp[$j] + 1);
+      }
+    }
+  }
+  echo max($dp);
+
+?>
