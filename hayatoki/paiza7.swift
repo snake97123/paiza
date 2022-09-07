836141912c14cdeaf25acbe6c9d2113acfe64d5e
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-09-08 07:12:42 +0900

    鉛筆の濃さ

---
+++ hayatoki/paiza7.swift
@@ -0,0 +1,3 @@
+let pencilDensity: Array<String> = ["6B", "5B", "4B", "3B", "2B", "B", "HB", "F", "H", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H"]
+let input = Int(readLine()!)!
+print(pencilDensity[input - 1])
