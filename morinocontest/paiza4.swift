74882bee48a5ea52c8e08c7292ebf926916551a2
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-09-17 06:47:43 +0900

    競輪のユニフォーム

---
+++ morinocontest/paiza4.swift
@@ -0,0 +1,4 @@
+let colorLineUp = ["white", "black", "red", "blue", "yellow", "green", "orange", "pink", "purple"]
+let n = Int(readLine()!)!
+let uniformColor = colorLineUp[n - 1]
+print(uniformColor)
