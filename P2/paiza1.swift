81399cb5d5ae0868da08fd018f18a8f8e0dfb9ed
snake97123 <73522198+snake97123@users.noreply.github.com>
2023-04-08 08:57:03 +0900

    拡散の脅威

---
+++ P2/paiza1.swift
@@ -0,0 +1,6 @@
+let retweetNumber = Int(readLine()!)!
+let averageFollowerNumberOfRetweet = Int(readLine()!)!
+let followerNumberOfTaro = Int(readLine()!)!
+
+let seeingTweetMaxNumber = retweetNumber * averageFollowerNumberOfRetweet + followerNumberOfTaro
+print(seeingTweetMaxNumber)
