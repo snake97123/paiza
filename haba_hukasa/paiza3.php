a93f707864c7f05606884f86d25217151e03bf52
snake97123 <73522198+snake97123@users.noreply.github.com>
2023-09-22 09:07:42 +0900

    Nマスの移動

---
+++ haba_hukasa/paiza3.php
@@ -0,0 +1,42 @@
+<?php 
+list($H, $W, $N) = array_map('intval', explode(" ", trim(fgets(STDIN))));
+$graph = [];
+for($i = 0; $i < $H; $i++) {
+  for($j = 0; $j < $W; $j++) {
+    $graph[$i][$j] = ".";
+  }
+}
+$distances = array_fill(0, $H, array_fill(0, $W, PHP_INT_MAX));
+$directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
+list($x, $y) = array_map('intval', explode(" ", trim(fgets(STDIN))));
+$queue = [[$x, $y, 0]];
+
+while(!empty($queue)) {
+  list($cx, $cy, $distance) = array_shift($queue);
+  if($distances[$cx][$cy] <= $distance) {
+    continue;
+  }
+  $distances[$cx][$cy] = $distance;
+
+  foreach($directions as $dir) {
+    $nx = $cx + $dir[0];
+    $ny = $cy + $dir[1];
+    if($nx >= 0 && $nx < $H && $ny >= 0 && $ny < $W && $distances[$nx][$ny] > $distance + 1) {
+      $queue[] = [$nx, $ny, $distance + 1];
+    }
+  }
+}
+
+for ($i = 0; $i < $H; $i++) {
+  for ($j = 0; $j < $W; $j++) {
+    echo ($distances[$i][$j] <= $N) ? "*" : ".";
+  }
+  echo "\n";
+}
+
+?>
+
+
+
+
